03999f246e58921d666b8cb57a2b5ebf87880949
syc1123 <874795069@qq.com>
2019-11-13 20:05:50 +0800

    软件构造实验一,解决字符串常量问题

--- test1/test.py
+++ test1/test.py
@@ -1,3 +1,5 @@
+import ctypes
+
 Identifier = 1  # 标识符 字母A-Z,a-z,0-9,_
 
 Constant = 2  # 常数，定点小数
@@ -102,10 +104,11 @@ def isNumber(index, data):
         return False
 
 
+# 处理连串的数字常量
 def dealNumber(builder):
     for i in builder:
         string = ''
-        deleteallindex = {}
+        deleteallindex = {}  # 要删除的所有下标
         deleteindex = []  # 记录要删除的下标
         for a in range(len(i)):
             if isNumber(a, i):
@@ -118,45 +121,39 @@ def dealNumber(builder):
                     deleteindex = []
         dellist = []
         for delallindex in deleteallindex.keys():
-            p = eval(delallindex)
+            p = eval(delallindex)  # 将列表的字符串形式转换回列表
             if len(p) != 1:
-                i[p[0]] = (1, deleteallindex[delallindex])
+                i[p[0]] = (2, deleteallindex[delallindex])
                 dellist += p[1:]
-        i = [i[x] for x in range(len(i)) if (x not in dellist)]
+        index = builder.index(i)
+        a = [i[x] for x in range(len(i)) if (x not in dellist)]
+        builder[index] = a
         dellist = []
         deleteallindex = {}
+
+
+def dealString(builder):
+    for X in builder:
+        id1 = [i for i, x in enumerate(X) if x[1] == '"']  # 查找有 " 的元素下标位置
+        dellist = []
+        for i in range(0, len(id1), 2):
+            string = ''
+            for delindex in range(id1[i] + 1, id1[i + 1]):
+                string = string + X[delindex][1]
+                if delindex != id1[i] + 1:
+                    dellist.append(delindex)
+            X[id1[i] + 1] = (2, string)
+        index = builder.index(X)
+        a = [X[x] for x in range(len(X)) if (x not in dellist)]
+        builder[index] = a
 
 
 if __name__ == '__main__':
     builder = []
     builder = dealProgrammer('data.txt')
-    # dealNumber(builder)
-    for i in builder:
-        # print(i)
-        string = ''
-        deleteallindex = {}
-        deleteindex = []  # 记录要删除的下标
-        for a in range(len(i)):
-            if isNumber(a, i):
-                string += i[a][1]
-                deleteindex.append(a)
-            else:
-                if string != '':
-                    deleteallindex.update({str(deleteindex): string})
-                    string = ''
-                    deleteindex = []
-        dellist = []
-        for delallindex in deleteallindex.keys():
-            p = eval(delallindex)
-            if len(p) != 1:
-                i[p[0]] = (1, deleteallindex[delallindex])
-                dellist += p[1:]
-        print(i)
-        i = [i[x] for x in range(len(i)) if (x not in dellist)]
-        print(i)
-        # dellist = []
-        # deleteallindex = {}
-    # for i in builder:
-    #     for j in i:
-    #         print(j, end="  ")
-    #     print()
+    dealNumber(builder)
+    dealString(builder)
+    for x in builder:
+        for j in x:
+            print(j, end=" ")
+        print()
