e72ef5d7fb6a86d80947926217a09b87461c827f
syc1123 <874795069@qq.com>
2019-11-14 11:54:24 +0800

    软件构造实验二

--- test2/__init__.py
+++ test2/__init__.py
@@ -4,6 +4,9 @@ ReservedWords = []
 TheOperator = {}
 RangeFlag = {}
 Number = {}
+Result = []
+Identifier = []  # 标识符 字母A-Z,a-z,0-9,_
+Constant = []  # 常数，定点小数
 
 
 def init():
@@ -19,7 +22,7 @@ def init():
     f.close()
     f = open('RangeFlag.txt', 'r')
     for line in f.readlines():
-        RangeFlaglist = line.split(',')
+        RangeFlaglist = line.split('-')
         for i in RangeFlaglist:
             RangeFlag[i] = 0
     f.close()
@@ -31,6 +34,13 @@ def init():
     f.close()
 
 
+def addelement(tu, element):
+    l = list(tu)
+    l.append(element)
+    tu = tuple(l)
+    return tu
+
+
 if __name__ == '__main__':
     init()
     builder = []
@@ -39,5 +49,31 @@ if __name__ == '__main__':
     dealString(builder)
     for x in builder:
         for j in x:
-            print(j, end=" ")
-        print()
+            if j[0] == 1:  # 标识符 字母A-Z,a-z,0-9,_
+                # print(1)
+                if j[1] not in Identifier:
+                    Identifier.append(j[1])
+                    j=addelement(j, Identifier.index(j[1]))
+                    Result.append(j)
+            elif j[0] == 2:  # 常数，定点小数
+                if j[1] not in Constant:
+                    Constant.append(j[1])
+                    addelement(j, Constant.index(j[1]))
+                    Result.append(j)
+                # print(2)
+            elif j[0] == 3:  # 保留字3
+                if j[1] not in Result:
+                    addelement(j, ReservedWords.index(j[1]))
+                    Result.append(j)
+                # print(3)
+            elif j[0] == 4:  # 运算符4
+                if j[1] not in Result:
+                    addelement(j, list(TheOperator).index(j[1]))
+                    Result.append(j)
+                # print(4)
+            else:  # 界符5
+                if j[1] not in Result:
+                    addelement(j, list(RangeFlag).index(j[1]))
+                    Result.append(j)
+                # print(5)
+        print(Result)
