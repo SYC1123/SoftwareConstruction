1bd87cc53d8b1eb7eb0f11c828215db2434822db
syc1123 <874795069@qq.com>
2019-11-15 19:20:34 +0800

    软件构造实验二结束！

--- test2/__init__.py
+++ test2/__init__.py
@@ -1,18 +1,38 @@
+import copy
+
 from test1.test import dealProgrammer, dealString, dealNumber
 
 ReservedWords = []
 TheOperator = {}
 RangeFlag = {}
 Number = {}
+# 以下为空
 Result = []
 Identifier = []  # 标识符 字母A-Z,a-z,0-9,_
 Constant = []  # 常数，定点小数
+ReservedWordsList = []  # 保留字
+TheOperatorList = []  # 运算符
+RangeFlagList = []  # 界符
 
 
-def init():
+def addelement(tu, element):
+    l = list(tu)
+    l.append(element)
+    tu = tuple(l)
+    return tu
+
+
+def findexist(j, Result):
+    for part in Result:
+        if j[1] == part[1]:
+            return part
+
+
+if __name__ == '__main__':
     f = open('ReservedWords.txt', 'r')
     for line in f.readlines():
-        ReservedWords = line.split(',')
+        NewReservedWords = line.split(',')
+        ReservedWords = copy.copy(NewReservedWords)
     f.close()
     f = open('TheOperator.txt', 'r')
     for line in f.readlines():
@@ -32,48 +52,61 @@ def init():
         for i in Numberlist:
             Number[i] = 0
     f.close()
-
-
-def addelement(tu, element):
-    l = list(tu)
-    l.append(element)
-    tu = tuple(l)
-    return tu
-
-
-if __name__ == '__main__':
-    init()
     builder = []
     builder = dealProgrammer('data.txt')
     dealNumber(builder)
     dealString(builder)
+    f=open('Result.txt','w')
     for x in builder:
+        ResultPart = []
         for j in x:
             if j[0] == 1:  # 标识符 字母A-Z,a-z,0-9,_
-                # print(1)
                 if j[1] not in Identifier:
                     Identifier.append(j[1])
-                    j=addelement(j, Identifier.index(j[1]))
+                    j = addelement(j, Identifier.index(j[1]))
                     Result.append(j)
+                    ResultPart.append(j)
+                else:
+                    part = findexist(j, Result)
+                    ResultPart.append(part)
             elif j[0] == 2:  # 常数，定点小数
                 if j[1] not in Constant:
                     Constant.append(j[1])
-                    addelement(j, Constant.index(j[1]))
+                    j = addelement(j, Constant.index(j[1]))
                     Result.append(j)
-                # print(2)
+                    ResultPart.append(j)
+                else:
+                    part = findexist(j, Result)
+                    ResultPart.append(part)
             elif j[0] == 3:  # 保留字3
-                if j[1] not in Result:
-                    addelement(j, ReservedWords.index(j[1]))
+                if j[1] not in ReservedWordsList:
+                    ReservedWordsList.append(j[1])
+                    j = addelement(j, ReservedWords.index(j[1]))
                     Result.append(j)
-                # print(3)
+                    ResultPart.append(j)
+                else:
+                    part = findexist(j, Result)
+                    ResultPart.append(part)
             elif j[0] == 4:  # 运算符4
-                if j[1] not in Result:
-                    addelement(j, list(TheOperator).index(j[1]))
+                if j[1] not in TheOperatorList:
+                    TheOperatorList.append(j[1])
+                    j = addelement(j, list(TheOperator).index(j[1]))
                     Result.append(j)
-                # print(4)
+                    ResultPart.append(j)
+                else:
+                    part = findexist(j, Result)
+                    ResultPart.append(part)
             else:  # 界符5
-                if j[1] not in Result:
-                    addelement(j, list(RangeFlag).index(j[1]))
+                if j[1] not in RangeFlagList:
+                    RangeFlagList.append(j[1])
+                    j = addelement(j, list(RangeFlag).index(j[1]))
                     Result.append(j)
-                # print(5)
-        print(Result)
+                    ResultPart.append(j)
+                else:
+                    part = findexist(j, Result)
+                    ResultPart.append(part)
+        print(ResultPart)
+        f.writelines(str(ResultPart))
+        f.writelines('\n')
+    print(Result)
+    f.close()
